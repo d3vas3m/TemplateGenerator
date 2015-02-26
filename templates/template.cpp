a9f2e2bce500176442994a97ea3d3eb6b7bcf633
ASEM DEVAJIT SINGH <asem.devajit@gmail.com>
2015-02-26 21:37:36 +0530

    Update and rename  to template.cpp

---
+++ templates/template.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <map>
+#define nl putchar('\n')
+#define V(type,name) vector<type> name
+#define FOR(i,x,n) for(i=x;i<n;i++)
+#define _FOR(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(),(v).end()
+#define display(v) for(typeof((v).begin()) it = (v).begin(); it != (v).end(); ++it) cout<<*it<<" "
+#define pb push_back
+#define sz(v) ( int )( (v).size() )
+#define cl(v) (v).clear()
+#define mod(a,b) ((a) % (b))
+const int MAX = (int)(1e6);
+using namespace std;
+
+char buffer[MAX];string gets(){scanf("%s",buffer);return buffer;}
+template<typename t> void get(t& a) { scanf("%d", &a); }
+template<typename t> void out(t& a) { printf("%d ",a); }
+template<typename t> void out(t& a, int New) { out(a); if(New) nl; }
+typedef map<int,int> MAP;
+typedef MAP::iterator iter;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    int i,t,n,k;
+    return 0;
+}
+
+//TemplateGenerator
