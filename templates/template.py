e7b9493e7f2a67e1bcde8564928d44110a0b505a
ASEM DEVAJIT SINGH <asem.devajit@gmail.com>
2015-02-26 21:42:08 +0530

    Update and rename templates to templates/template.py

---
+++ templates/template.py
@@ -0,0 +1,11 @@
+__author__ = "root"
+__prog__   = ""
+
+import sys, math
+
+def main():
+    t = int(sys.stdin.readline())
+    for i in xrange(t):
+        n = sys.stdin.readline().strip()
+if __name__ == "__main__":
+    main()
